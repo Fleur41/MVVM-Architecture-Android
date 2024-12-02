19ec4acf3f0e6b5d5d412759b6034ce601a6be80
Fleur41 <Muriithikings456@gmail.com>
2024-12-02 16:56:14 +0300

    First commit

---
+++ app/src/main/java/com/simon/mvvm_architecture_android/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.simon.mvvm_architecture_android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.simon.mvvm_architecture_android.ui.theme.MVVMArchitectureAndroidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MVVMArchitectureAndroidTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MVVMArchitectureAndroidTheme {
+        Greeting("Android")
+    }
+}
